85d7a01b29453b840ba55cda93b61e3285c8d94d
Jonathan Verschaeve <114299902+jlv94@users.noreply.github.com>
2024-11-21 14:43:35 -0800

    Add files via upload

---
+++ js/faq_open.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.faq-question').forEach(question => {
+        question.addEventListener('click', () => {
+            const answer = question.nextElementSibling;
+            answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+        });
+    });
+});
